4af5e8692f02133543a5d64c8553feca936a03a6
swy <minicr1234@gmail.com>
2025-04-12 22:13:17 +0800

    c(style): history preview title to custom-p-font-size
    一開始打開roocode的歷史頁面

--- webview-ui/src/components/history/HistoryPreview.tsx
+++ webview-ui/src/components/history/HistoryPreview.tsx
@@ -7,7 +7,7 @@ const HistoryPreview = () => {
 	const { tasks } = useTaskSearch()
 
 	return (
-		<div className="flex flex-col gap-3">
+		<div className="flex flex-col gap-3" style={{ fontSize: "var(--custom-p-font-size)" }}>
 			{tasks.length !== 0 && (
 				<>
 					{tasks.slice(0, 3).map((item) => (
